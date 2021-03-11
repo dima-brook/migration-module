6122b05ea24f98c6a31fa8bd225a14f1168869f0
Vkint <kint@xpdiem.com>
2021-03-11 13:37:39 +0200

    added mint function with limited

--- src/services/solidityMigration/contracts/solidityContractGenerator.ts
+++ src/services/solidityMigration/contracts/solidityContractGenerator.ts
@@ -3,7 +3,7 @@ import { ISolidityGenrator } from '../types'
 export default (solidityGenerator: ISolidityGenrator) => {
 
 }
-
+// generate nft for user of user
 const createNFT = (solidityGenerator: ISolidityGenrator) => {
     const {
         name,
@@ -15,12 +15,30 @@ const createNFT = (solidityGenerator: ISolidityGenrator) => {
 import "./openZeppelin/ERC721.sol";
 contract ${name} is ERC721 {
     ${NFTLimit(limited)}
+    uint256 public count = 0;
+    mapping(uint256 => string) public tokenURIs;
     constructor() ERC721("${name}", "${symbol}") public {
+
     }
+    ${mint(solidityGenerator)}
 }`
 }
 
-
+// limit the creation of nft
 const NFTLimit = (limited: number) => `uint256 public limited = ${limited};
-uint256 public count = 0;
 `
+
+const mint = (solidityGenerator: ISolidityGenrator) => {
+    const { 
+        limited,
+        limitedMessage
+    } = solidityGenerator
+    return `
+    mint(string memory _uri) public {
+        ${limited ? `require(count < limited, '${limitedMessage}');` : ''}
+        tokenURIs[count] = _uri;
+        _mint(msg.sender, count);   
+        count ++;
+    }    
+`
+}
