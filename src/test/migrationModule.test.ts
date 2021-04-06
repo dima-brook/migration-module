dbc1149104015111fdfae73eeb841e7a601e17e1
Vkint <kint@xpdiem.com>
2021-04-06 17:02:31 +0300

    init migration module tests

--- src/test/migrationModule.test.ts
+++ src/test/migrationModule.test.ts
@@ -1,7 +1,8 @@
+import { assert, expect } from 'chai'
 import { UseCaseEnum } from '../enums/useCasesEnum'
 import { MigrationModule } from '../services/migrationModule'
 import { IGenerator } from '../services/migrationModule/types'
-
+import { GeneratorConfig, ILib } from '../tools/fileGenerator'
 const generator: IGenerator = {
     solidity: {
         name: 'TestContract',
@@ -17,7 +18,19 @@ const generator: IGenerator = {
 
 const MIGRATION_MODULE = new MigrationModule(generator)
 
-describe('Migration Module', async () => {
-    const solidity = await MIGRATION_MODULE.generateSolidityContracts()
-    console.log(solidity)
+describe('Migration Module', async function() {
+    it('Generates a solidity smart contract', async function() {
+        const solidity: GeneratorConfig | undefined = await MIGRATION_MODULE
+            .generateSolidityContracts()
+        if(solidity) {
+            const { folders, libs } = solidity
+            expect(folders).to.be.an('array')
+            expect(libs).to.be.an('array')
+            folders.forEach((n: string) => expect(n).to.be.a('string'))
+            libs.forEach((n: ILib) => {
+                expect(n.code).to.be.a('string')
+                expect(n.location).to.be.a('string')
+            })
+        } else assert.fail()
+    })
 })
